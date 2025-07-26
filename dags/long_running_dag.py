7eab7d431511527341f9b09a8b162a865e0c21cc
bench87 <bench8704@gmail.com>
2025-07-26 07:51:47 +0000

    change schedule key

--- dags/long_running_dag.py
+++ dags/long_running_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     "long_running_dag",
     start_date=datetime(2021, 1, 1),
-    schedule_interval="* * * * *",
+    schedule="* * * * *",
     catchup=False,
 ) as dag:
     task_1 = BashOperator(task_id="task_1", bash_command="sleep 350")
